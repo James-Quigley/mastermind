28335ea3ca178518a55e5cce7949b312d4c58445
James Quigley <James.A.Quigley@asu.edu>
2019-01-27 16:05:02 -0700

    Animate win/loss text

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { compareGuess, generateCode } from './helpers';
+import { compareGuess, COLORS, generateCode } from './helpers';
 import './peg.css';
 
 export default () => {
@@ -13,49 +13,58 @@ export default () => {
 
     const [history, setHistory] = React.useState<Array<{guess: Array<string>, result: { black: number, white: number }}>>([]);
 
-    const [first, setFirst] = React.useState<string>("BLACK");
-    const [second, setSecond] = React.useState<string>("BLACK");
-    const [third, setThird] = React.useState<string>("BLACK");
-    const [fourth, setFourth] = React.useState<string>("BLACK");
+    const [first, setFirst] = React.useState<string>(COLORS.BLACK);
+    const [second, setSecond] = React.useState<string>(COLORS.BLACK);
+    const [third, setThird] = React.useState<string>(COLORS.BLACK);
+    const [fourth, setFourth] = React.useState<string>(COLORS.BLACK);
+
+    const [winClass, setWinClass] = React.useState('');
+    const [lossClass, setLossClass] = React.useState('');
 
     return (
         <div>
           <div>
-            <p>Wins: {wins}</p>
-            <p>Losses: {losses}</p>
+            <p style={{ lineHeight: '2em' }}>Wins: <span className={winClass} style={{
+              transition: "font-size .75s",
+              lineHeight: '2em'
+            }}>{wins}</span></p>
+            <p style={{ lineHeight: '2em' }}>Losses: <span className={lossClass} style={{
+              transition: "font-size .75s",
+              lineHeight: '2em'
+            }}>{losses}</span></p>
           </div>
             <div>
                 <select name="1" id="1" onChange={(e) => setFirst(e.target.value)}>
-                  <option value="BLACK">Black</option>
-                  <option value="BLUE">Blue</option>
-                  <option value="GREEN">Green</option>
-                  <option value="RED">Red</option>
-                  <option value="WHITE">White</option>
-                  <option value="YELLOW">Yellow</option>
+                  <option value={COLORS.BLACK}>Black</option>
+                  <option value={COLORS.BLUE}>Blue</option>
+                  <option value={COLORS.GREEN}>Green</option>
+                  <option value={COLORS.RED}>Red</option>
+                  <option value={COLORS.WHITE}>White</option>
+                  <option value={COLORS.YELLOW}>Yellow</option>
                 </select>
                 <select name="2" id="2" onChange={(e) => setSecond(e.target.value)}>
-                  <option value="BLACK">Black</option>
-                  <option value="BLUE">Blue</option>
-                  <option value="GREEN">Green</option>
-                  <option value="RED">Red</option>
-                  <option value="WHITE">White</option>
-                  <option value="YELLOW">Yellow</option>
+                  <option value={COLORS.BLACK}>Black</option>
+                  <option value={COLORS.BLUE}>Blue</option>
+                  <option value={COLORS.GREEN}>Green</option>
+                  <option value={COLORS.RED}>Red</option>
+                  <option value={COLORS.WHITE}>White</option>
+                  <option value={COLORS.YELLOW}>Yellow</option>
                 </select>
                 <select name="3" id="3" onChange={(e) => setThird(e.target.value)}>
-                  <option value="BLACK">Black</option>
-                  <option value="BLUE">Blue</option>
-                  <option value="GREEN">Green</option>
-                  <option value="RED">Red</option>
-                  <option value="WHITE">White</option>
-                  <option value="YELLOW">Yellow</option>
+                  <option value={COLORS.BLACK}>Black</option>
+                  <option value={COLORS.BLUE}>Blue</option>
+                  <option value={COLORS.GREEN}>Green</option>
+                  <option value={COLORS.RED}>Red</option>
+                  <option value={COLORS.WHITE}>White</option>
+                  <option value={COLORS.YELLOW}>Yellow</option>
                 </select>
                 <select name="4" id="4" onChange={(e) => setFourth(e.target.value)}>
-                  <option value="BLACK">Black</option>
-                  <option value="BLUE">Blue</option>
-                  <option value="GREEN">Green</option>
-                  <option value="RED">Red</option>
-                  <option value="WHITE">White</option>
-                  <option value="YELLOW">Yellow</option>
+                  <option value={COLORS.BLACK}>Black</option>
+                  <option value={COLORS.BLUE}>Blue</option>
+                  <option value={COLORS.GREEN}>Green</option>
+                  <option value={COLORS.RED}>Red</option>
+                  <option value={COLORS.WHITE}>White</option>
+                  <option value={COLORS.YELLOW}>Yellow</option>
                 </select>
                 <button disabled={gameOver} onClick={(e) => {
                     const result = compareGuess(code, [first, second, third, fourth]);
@@ -66,19 +75,23 @@ export default () => {
 
                     if (result.black === 4){
                       setGameOver(true);
+                      setWinClass('big-text');
                       setWins(wins + 1);
                       setTimeout(() => {
                         setCode(generateCode());
                         setHistory([]);
                         setGameOver(false);
+                        setWinClass('');
                       }, 2000);
                     } else if (history.length >= 9){
                       setGameOver(true);
+                      setLossClass('big-text');
                       setLosses(losses + 1);
                       setTimeout(() => {
                         setCode(generateCode());
                         setHistory([]);
                         setGameOver(false);
+                        setLossClass('');
                       }, 2000);
                     }
                 }}>Guess</button>
